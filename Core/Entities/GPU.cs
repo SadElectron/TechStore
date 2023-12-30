6599aaefa6aba533253573537673a32f1fa59fbc
Sadman <Sadman@DESKTOP-QIBKRE7>
2023-12-30 05:34:59 +0300

    Add project files.

---
+++ Core/Entities/GPU.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class GPU
+    {
+        public Guid ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string ChipsetBrand { get; set; }
+        public string Chipset { get; set; }
+        public int Memory { get; set; }
+        public int Clock { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
